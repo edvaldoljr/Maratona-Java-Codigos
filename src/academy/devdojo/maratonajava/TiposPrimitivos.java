8cec46e3d3ab45a72045b3fe365ae3c562ae86bd
edvaldoljr <edvaldoluizj@gmail.com>
2021-09-03 20:11:42 -0300

    TiposPrimitivos

---
+++ src/academy/devdojo/maratonajava/TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+
+            boolean casadoVerdadeiro = true;
+            boolean solteiroFalso = false;
+            char caractere = 'A';
+            char caractereHexadecimal = '\u0041';
+            String nomeAluno = "Edvaldo";
+            byte idadeByte = 127;
+            short idadeShort = 32767;
+            int idadeDoAluno = 2147483;
+            long numeroGrande = 263;
+            float pontoFlutuante = 3.40282346638528860F;
+            double pontoFlutuanteDobroDeCapacidade = 1.79769313486231570D;
+        }
+    }
+
