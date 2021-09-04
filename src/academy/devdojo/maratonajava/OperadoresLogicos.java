3049470c3e159b8bd9e2f334ba18bd64c6eabf92
edvaldoljr <edvaldoluizj@gmail.com>
2021-09-03 21:47:42 -0300

    Operadores Logicos

---
+++ src/academy/devdojo/maratonajava/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        // && (AND)
+        // || (OU)
+        int idade = 18;
+        double altura = 1.60;
+        System.out.println(idade >= 18 && altura >= 1.60);
+    }
+}
