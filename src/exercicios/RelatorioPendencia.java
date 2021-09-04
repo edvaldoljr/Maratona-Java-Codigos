d3c21f38d532657150bb79f5ab58f17065ccfe1e
edvaldoljr <edvaldoluizj@gmail.com>
2021-09-03 21:17:25 -0300

    Exercicio Relatorio de Pendencia

---
+++ src/exercicios/RelatorioPendencia.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+public class RelatorioPendencia {
+    public static void main(String[] args) {
+        /**
+         * Crie um algoritmo que possua variaveis para salvar os seguintes dados: Nome,Endereco e Telefone.
+         * Emprima da seguinte forma ; O <nome> domiciliado no endereço <endereço>,
+         * eo telefone <telefone> não possui pendencias.
+         */
+        String nome = "Edvaldo";
+        String endereco = "Rua Vila Real n° 18";
+        String telefone = "(19) 9.8831-3921";
+        System.out.println("O " + nome + ", domiciliado no endereço " + endereco +
+                " eo telefone " + telefone + " não possui pendencias.");
+    }
+}
