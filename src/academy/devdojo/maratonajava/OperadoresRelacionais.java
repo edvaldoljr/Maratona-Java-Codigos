212a937ee504c3d3b95352baa3d38fd8a1173c16
edvaldoljr <edvaldoluizj@gmail.com>
2021-09-03 21:42:51 -0300

    Operadores Relacionais

---
+++ src/academy/devdojo/maratonajava/OperadoresRelacionais.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava;
+
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        boolean dezMaiorQueVinte = 10 > 20;
+        System.out.println(dezMaiorQueVinte);
+
+        boolean dezMaiorOuIgual = 10 >= 20;
+        System.out.println(dezMaiorOuIgual);
+
+        boolean dezMenorOuIgual = 10 <= 20;
+        System.out.println(dezMenorOuIgual);
+
+        boolean dezMenorQueVinte = 10 < 20;
+        System.out.println(dezMenorQueVinte);
+
+        System.out.println(5 == 5);
+
+        System.out.println(5 != 5);
+    }
+}
+
