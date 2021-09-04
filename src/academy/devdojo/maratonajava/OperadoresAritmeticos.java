2157cc11b2d52c7e91579b57945227ff5f84ce1b
edvaldoljr <edvaldoluizj@gmail.com>
2021-09-03 21:38:19 -0300

    Operadores Aritmeticos

---
+++ src/academy/devdojo/maratonajava/OperadoresAritmeticos.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        // Operadores  + - / *
+        int a = 30;
+        int b = 5;
+        int c = 10;
+        int total = (a + b + c) / 10;
+        System.out.println("O resultado = "+total);
+
+        // Troque os operadores um a um e veja os resultados.
+    }
+}
