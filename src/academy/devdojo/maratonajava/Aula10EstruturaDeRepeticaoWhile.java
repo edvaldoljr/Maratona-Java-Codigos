bd9de0d7c3b02bd4d8ae8bfc42f270f79bd2df55
edvaldoljr <edvaldoluizj@gmail.com>
2023-05-26 03:59:04 -0300

    AULA 10 e 11 Estrutura de repeticao while e do while

---
+++ src/academy/devdojo/maratonajava/Aula10EstruturaDeRepeticaoWhile.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava;
+
+public class Aula10EstruturaDeRepeticaoWhile {
+
+    public static void main(String[] args) {
+
+        int cont = 0;
+        while (cont < 10) {
+            System.out.println(++cont);
+        }
+    }
+}
---
+++ src/academy/devdojo/maratonajava/Aula11EstruturaDeRepeticaoDoWhile.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava;
+
+public class Aula11EstruturaDeRepeticaoDoWhile {
+
+    public static void main(String[] args) {
+
+        int cont = 0;
+
+        do {
+            System.out.println(++cont);
+        } while (cont < 10);
+    }
+}
